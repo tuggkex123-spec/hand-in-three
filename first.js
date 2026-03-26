061eaf5964f3aa2ad10683592ebac13724da638c
Julia Palm <julia.palm@code.berlin>
2026-03-26 14:03:21 +0100

    Tried starting a database

--- first.js
+++ first.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 const PORT = 3000
+
+mongoose.connect('mongodb://127.0.0.1:27017/admin')
 
 app.listen(PORT, () => {
   console.log(`👋 Started server on port ${PORT}`)
