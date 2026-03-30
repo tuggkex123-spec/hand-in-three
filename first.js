3a182ad50b0b9be3a9c0b40d8fd85a1ce1c0f7d6
Julia Palm <julia.palm@code.berlin>
2026-03-30 14:11:36 +0200

    Trying to add a form

--- first.js
+++ first.js
@@ -5,7 +5,37 @@ const app = express()
 const PORT = 3000
 
 mongoose.connect('mongodb://127.0.0.1:27017/admin')
+  .then(() => console.log('💽 Database connected'))
+  .catch(error => console.error(error))
 
 app.listen(PORT, () => {
   console.log(`👋 Started server on port ${PORT}`)
+})
+
+const userSchema = new mongoose.Schema({
+  slug: { type: String, unique: true, required: true },
+  user_name: { type: String, unique: true, required: true },
+  age: { type: String, unique: true, required: true }, 
+})
+
+const User = mongoose.model('User', userSchema)
+
+app.post('/users', async (request, response) => {
+  try {
+    const user = new User({
+    slug: 'jane-doe',
+    user_name: 'Jane Doe',
+    age: 22
+  })
+  await user.save()
+
+    response.send('User Created')
+  }catch (error) {
+    console.error(error)
+    response.send('Error: The user could not be created.')
+  }
+})
+
+app.get('/user/new', (request, response) => {
+  response.render('users/new')
 })
