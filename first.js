42bb8486985aa86c2e109582a5447245454aead3
Julia Palm <julia.palm@code.berlin>
2026-03-30 14:58:13 +0200

    Made the form work

--- first.js
+++ first.js
@@ -5,10 +5,10 @@ const app = express()
 const PORT = 3000
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/admin')
   .then(() => console.log('💽 Database connected'))
