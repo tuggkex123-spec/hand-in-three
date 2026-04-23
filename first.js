86064f646b14f18020288af69186ef804fd631d2
Julia Palm <julia.palm@code.berlin>
2026-04-23 14:28:16 +0200

    added a custom error class

--- first.js
+++ first.js
@@ -215,3 +215,11 @@ app.use((error, request, response, next) => {
   // Fallback
   response.status(500).send('Something went wrong.')
 })
+
+class AppError extends Error {
+  constructor(message, statusCode = 500) {
+    super(message)
+    this.statusCode = statusCode
+    this.name = 'AppError'
+  }
+}
