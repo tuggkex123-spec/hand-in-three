e88f774acd5473673ccce5e2acf45dc124e18f47
Julia Palm <julia.palm@code.berlin>
2026-05-12 14:28:25 +0200

    had the wrong name for the submit button in my ui test

--- tests/users.ui.test.js
+++ tests/users.ui.test.js
@@ -7,5 +7,6 @@ test('users/new page loads and form is usable', async ({ page }) => {
   await expect(page.locator('input[name="slug"]')).toBeVisible()
   await expect(page.locator('input[name="user_name"]')).toBeVisible()
   await expect(page.locator('input[name="age"]')).toBeVisible()
-  await expect(page.locator('button[type="submit"]')).toBeVisible()
+  await expect(page.locator('button')).toBeVisible()
+
 })
