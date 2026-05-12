59f59b22ffb1fea702f8b3232b6c6839be63eae1
Julia Palm <julia.palm@code.berlin>
2026-05-12 14:03:08 +0200

    there was no working DB connection so had to change the api test

--- tests/users.api.test.js
+++ tests/users.api.test.js
@@ -2,8 +2,8 @@ import request from 'supertest'
 import app from '../app.js'
 
 describe('Users API', () => {
-  test('GET /users returns status 200', async () => {
-    const response = await request(app).get('/users')
+  test('GET /users/new returns status 200', async () => {
+    const response = await request(app).get('/users/new')
 
     expect(response.status).toBe(200)
   })
